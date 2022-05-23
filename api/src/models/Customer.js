9e2419e4afe8b1d809ef0e5bf2b2b3d881fefeb5
zayrarepositor <zayra.contacto@gmail.com>
2022-05-23 16:59:31 -0300

    update (controllers/models): update customer model
    last_name is required

--- api/src/models/Customer.js
+++ api/src/models/Customer.js
@@ -23,6 +23,7 @@ module.exports = (sequelize) => {
       },
       last_name: {
         type: DataTypes.STRING,
+        required: true,
       },
       city: {
         type: DataTypes.STRING,
@@ -36,10 +37,6 @@ module.exports = (sequelize) => {
         type: DataTypes.STRING,
         required: true,
       },
-      /* QUESTIONS 
-      postal_code 
-      state_name
-      is necessary??*/
       postal_code: {
         type: DataTypes.STRING,
         required: true,
@@ -48,11 +45,6 @@ module.exports = (sequelize) => {
         type: DataTypes.STRING,
         required: true,
       },
-      /* CHANGES:
-      isAdmin => added
-      isPremium => name modified
-      isBanned => name modified
-         */
       isAdmin: {
         type: DataTypes.BOOLEAN,
         defaultValue: false,
