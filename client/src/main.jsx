a5225e77c3475318006769e0e97a20149410196c
Patricio Stickar <58763654+patostickar@users.noreply.github.com>
2022-05-24 16:54:32 -0300

    style(client/ index, main, App) added material base imports

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,13 @@ import { Provider } from 'react-redux';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
+import CssBaseline from '@mui/material/CssBaseline';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <CssBaseline />
         <App />
       </PersistGate>
     </Provider>
