efa49bea209a985872277da23c43ee00102250f9
Cesar Galeano <tocega@yahoo.es>
2022-05-25 16:46:33 -0500

    Inclusion foto Profile

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
-import Navbar from '../components/Navbar';
+import { Navbar } from '../components/Navbar.jsx';
 import './styles/Home.module.css';
 
 const Home = () => {
