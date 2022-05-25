cbc9794e0ac6ecdc1e67b08afcf9cd64d18392a5
Patricio Stickar <58763654+patostickar@users.noreply.github.com>
2022-05-25 15:18:12 -0300

    refactor(client/ Home): removed Home component, imports update

--- client/src/components/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import  { Navbar } from './Navbar.jsx';
-
-export const Home = () => {
-  return (
-    <Navbar />
-  )
-}
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import SearchBar from '../../components/SearchBar';
+import Navbar from '../../components/Navbar';
 import '../styles/Home.module.css';
 
 const Home = () => {
   return (
     <div>
+      <Navbar />
       <SearchBar />
       {/*
     navbar
