037c7e87352f94b1e3f7e97a74019b3481083a78
franco garcia <96747065+Dakkai@users.noreply.github.com>
2022-05-24 21:03:41 -0300

    ruta get y post de car refactorizadas y arregladas

--- api/src/controllers/getCars.js
+++
@@ -1,8 +0,0 @@
-// const { getCarsApi } = require('../services/getCars');
-const {Car} = require("../db")
-
-module.exports.getCars = async ()=>{
-   const cars = await Car.findAll()
-   console.log(cars)
-   return cars
-  }
