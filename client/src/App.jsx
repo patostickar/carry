5e2c3a0f561b24315bebbc5e1b80d7d569d82646
ByronCorrea <byron10-@hotmail.com>
2022-05-26 18:06:28 -0300

    Created Profile route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Booking } from './components/Booking.jsx';
-import Home from './pages/Home';
-import SearchList from './pages/SearchList';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Booking } from "./components/Booking.jsx";
+import Home from "./pages/Home";
+import SearchList from "./pages/SearchList";
+import "./App.css";
+import Profile from "./components/Profile.jsx";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/home' element={<Home />} />
-          <Route path='/booking' element={<Booking />} />
-          <Route path='/searchResult' element={<SearchList />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/searchResult" element={<SearchList />} />
         </Routes>
       </BrowserRouter>
     </>
