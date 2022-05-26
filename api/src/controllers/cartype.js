a1dbda2e13c98ddc7ab1f8ff6d6d48aead755e53
franco garcia <96747065+Dakkai@users.noreply.github.com>
2022-05-26 10:58:12 -0300

    arreglado

--- api/src/controllers/cartype.js
+++ api/src/controllers/cartype.js
@@ -1,9 +1,13 @@
 const { getTypeDB } = require("../services/cartypes");
 
 const getType =  async (req,res,next) =>{
-    const {id} = req.params
-    const data = await getTypeDB(id)
-    res.send(data)
+    try {
+        const {id} = req.params
+        const data = await getTypeDB(id)
+        res.send(data)
+    } catch (error) {
+        next(error)
+    }
 
 };
 module.exports = {
--- api/src/services/cartypes.js
+++ api/src/services/cartypes.js
@@ -1,24 +1,9 @@
-const { Car, Location, Cartype } = require("../db");
+const { Cartype } = require("../db");
 
 const getTypeDB = async (id) => {
     if(id){return await Cartype.findOne({where: { id: id }})}
     const types = await Cartype.findAll()
-   const typesMap = types.map(type =>{return{
-           id: type.id,
-           make: type.make,
-           model: type.model,
-           classCode: type.classCode,
-           className: type.className,
-           transmission: type.transmission ,
-           mpg: type.mpg ,
-           img: type.img,
-           doors: type.doors,
-           seats: type.seats,
-           airConditioning: type.air_conditioning,
-           largeSuitcase: type.large_suitcase,
-           smallSuitcase: type.small_suitcase}
-    })
-    return typesMap
+    return types
 }
 
 
