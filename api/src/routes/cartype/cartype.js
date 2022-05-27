bacbee7cc71dc972d86c76f80c595e553ff5d96d
franco garcia <96747065+Dakkai@users.noreply.github.com>
2022-05-27 09:59:23 -0300

    refactor de cartype count algunos archivos cambairon de nombre

--- api/src/routes/cartype/cartype.js
+++ api/src/routes/cartype/cartype.js
@@ -1,24 +1,12 @@
+
 const { Router } = require('express');
-const { getType } = require('../../controllers/cartype');
+const { getType, GetTypeConunt } = require('../../controllers/cartype');
 const router = Router();
-const { Cartype, Car } = require('../../db.js');
 
 router.get('/', getType);
-router.get('/count/:locationId', async (req, res, next) => {
-  const { locationId } = req.params;
-  try {
-    const count = await Car.count({
-      attributes: ['cartypeId'],
-      where: { locationId },
-      group: 'cartypeId',
-    });
-
-    res.send(count);
-  } catch (err) {
-    console.log(err);
-  }
-});
+router.get('/count/:locationId',GetTypeConunt);
 router.get('/:id', getType);
+
 
 router.put('/:id');
 router.patch('/:id');
