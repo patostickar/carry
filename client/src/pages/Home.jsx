63a11527f23d43eedd01b8c5e994574ae52a2c00
ByronCorrea <byron10-@hotmail.com>
2022-05-26 23:55:50 -0300

    Add TabTitle

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import SearchBar from '../components/SearchBar/SearchBar';
-import { Navbar } from '../components/Navbar';
-import './styles/Home.module.css';
-import PopularLocations from "../components/PopularLocations"
+import React from "react";
+import SearchBar from "../components/SearchBar/SearchBar";
+import { Navbar } from "../components/Navbar";
+import "./styles/Home.module.css";
+import PopularLocations from "../components/PopularLocations";
+import { TabTitle } from "../components/GeneralFuntions/GeneralFuntions";
 
 const Home = () => {
+  TabTitle("Inicio - Carry");
   return (
     <div>
       <Navbar />
