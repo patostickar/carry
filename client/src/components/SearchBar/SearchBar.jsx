da6279f0a4cb894e3bd5db3d95be962be8794ece
Patricio Stickar <58763654+patostickar@users.noreply.github.com>
2022-06-02 12:28:36 -0300

    refactor(client): same return moved to searchBar component

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,13 @@
 import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchCarTypes } from '../../redux/carsResults';
+import { setSameLocation } from '../../redux/searchBar.js';
 import Location from './locationInput';
 import Calendar from './calendar';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
 import styles from './styles/SearchBar.module.css';
 
 function SearchBar() {
@@ -18,6 +23,11 @@ function SearchBar() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const handleChange = (event) => {
+    const sameLocation = event.target.value === 'true';
+    dispatch(setSameLocation(sameLocation));
+  };
+
   const handleSearch = () => {
     if (!(pickupLocation && dropoffLocation)) {
       return alert('Por favor complete los campos de búsqueda');
@@ -30,20 +40,42 @@ function SearchBar() {
   };
 
   return (
-    <div className={styles.headerSearch}>
-      <div className={styles.headerSearchItem}>
-        <Location type='Retiro' popLocation={popLocation} />
+    <>
+      <FormControl>
+        <RadioGroup
+          row
+          aria-labelledby='demo-controlled-radio-buttons-group'
+          name='controlled-radio-buttons-group'
+          value={sameLocation}
+          onChange={handleChange}
+        >
+          <FormControlLabel
+            value='true'
+            control={<Radio />}
+            label='Devolver en el mismo lugar'
+          />
+          <FormControlLabel
+            value='false'
+            control={<Radio />}
+            label='Devolver en otro lugar'
+          />
+        </RadioGroup>
+      </FormControl>
+      <div className={styles.headerSearch}>
+        <div className={styles.headerSearchItem}>
+          <Location type='Retiro' popLocation={popLocation} />
+        </div>
+        <div className={styles.headerSearchItem}>
+          <Location type='Devolución' sameLocation={sameLocation} />
+        </div>
+        <Calendar />
+        <div className={styles.headerSearchItem}>
+          <button className={styles.siCheckButton} onClick={handleSearch}>
+            Buscar
+          </button>
+        </div>
       </div>
-      <div className={styles.headerSearchItem}>
-        <Location type='Devolución' sameLocation={sameLocation} />
-      </div>
-      <Calendar />
-      <div className={styles.headerSearchItem}>
-        <button className={styles.siCheckButton} onClick={handleSearch}>
-          Buscar
-        </button>
-      </div>
-    </div>
+    </>
   );
 }
 
