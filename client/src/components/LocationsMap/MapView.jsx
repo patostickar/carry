cafb8bdd8a18cd4dc2724a9bc16c1d0debc66b89
franco garcia <96747065+Dakkai@users.noreply.github.com>
2022-06-02 11:03:06 -0300

    ahora en el mapa solo se muestran las ubicaciones seleccionadas por el usuario

--- client/src/components/LocationsMap/MapView.jsx
+++ client/src/components/LocationsMap/MapView.jsx
@@ -13,16 +13,17 @@ function MapFunction(props){
 }
 
 export default  function MapView() {
-  const { locations } = useSelector(
-    (state) => state.searchBar
-  );
-  const { pickupLocation } = useSelector((state) => state.searchBar);
+  const { pickupLocation , dropoffLocation } = useSelector((state) => state.searchBar);
   const [actual, setActual] = useState(pickupLocation);
+  const markers = [pickupLocation , dropoffLocation]
   
-  
+
   useEffect(() => {
     setActual(pickupLocation)
   }, [pickupLocation])
+  useEffect(() => {
+    setActual(dropoffLocation)
+  }, [dropoffLocation])
   
 
 
@@ -39,24 +40,27 @@ export default  function MapView() {
   
     return (
       <div style={{width:"100%",height:"100%"}}>
-      <MapContainer center={pickupLocation?.geo} zoom={13} style={{width:"100%",height:"100%"}} >
+      <MapContainer center={pickupLocation?.geo} zoom={15} style={{width:"100%",height:"100%"}} >
       <TileLayer
         // attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
         />
-       {locations?.map(location => (
+       {markers?.map(location => (
+       <>
+       {location &&  
          <Marker
-         key={location.id}
+         key={location?.id}
          position={[
-           location.geo[0],
-           location.geo[1],
+           location?.geo[0]|| 0,
+           location?.geo[1]|| 0,
           ]}
           icon={LocationIcon}>
             <MapFunction actual={actual}/>
             <Popup>
-              <div>{location.name}</div>
+              <div>{location?.name}</div>
               </Popup>
-             </Marker>
+             </Marker>}
+            </>
           ))}
     </MapContainer>
         </div>
