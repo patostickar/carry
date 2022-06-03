4049d59db4d173f955cee148922444cc2519d51e
Patricio Stickar <58763654+patostickar@users.noreply.github.com>
2022-06-03 04:05:12 -0300

    feature(api): avoid duplicate carTypes

--- api/src/models/CarType.js
+++ api/src/models/CarType.js
@@ -12,54 +12,62 @@ module.exports = (sequelize) => {
       make: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: 'uniqueCarType',
       },
       model: {
         type: DataTypes.STRING,
         allowNull: false,
-      },
-      classCode: {
-        type: DataTypes.STRING,
-        allowNull: false,
+        unique: 'uniqueCarType',
       },
       className: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: 'uniqueCarType',
       },
       transmission: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: 'uniqueCarType',
       },
       mpg: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: 'uniqueCarType',
       },
       img: {
         type: DataTypes.STRING,
         allowNull: false,
+        unique: 'uniqueCarType',
       },
       doors: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        unique: 'uniqueCarType',
       },
       seats: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        unique: 'uniqueCarType',
       },
       airConditioning: {
         type: DataTypes.BOOLEAN,
         defaultValue: true,
+        unique: 'uniqueCarType',
       },
       largeSuitcase: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        unique: 'uniqueCarType',
       },
       smallSuitcase: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        unique: 'uniqueCarType',
       },
       price: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        unique: 'uniqueCarType',
       },
     },
     { timestamps: false }
