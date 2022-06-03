e38670c58ec509bbdca7b60868be3481e04268a0
franco garcia <96747065+Dakkai@users.noreply.github.com>
2022-06-03 20:36:18 -0300

    refactor alerts en funcion

--- client/src/components/GeneralFuntions/GeneralFuntions.jsx
+++ client/src/components/GeneralFuntions/GeneralFuntions.jsx
@@ -1,6 +1,23 @@
 // Usar este componente para agregar funciones generales a todos los componentes
+import Swal from 'sweetalert2'
+import 'sweetalert2/dist/sweetalert2.css'
 
 export const TabTitle = (newTitle) => {
   return (document.title = newTitle);
 };
 
+export const Alerts = (icon,title)=>{
+  Swal.fire({
+    position: 'top-end',
+    toast:true,
+    icon,
+    title,
+    showConfirmButton: false,
+    timer: 1500,
+    timerProgressBar: true,
+    didOpen: (toast) => {
+      toast.addEventListener('mouseenter', Swal.stopTimer)
+      toast.addEventListener('mouseleave', Swal.resumeTimer)
+    }
+  })
+}
