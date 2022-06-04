aeaf0be5a9dac70638a7e242981d8eec65a53754
Patricio Stickar <58763654+patostickar@users.noreply.github.com>
2022-06-04 13:37:49 -0300

    removed console.logs

--- client/src/components/Home/Testimonials.jsx
+++ client/src/components/Home/Testimonials.jsx
@@ -13,9 +13,7 @@ import { useEffect } from 'react';
 
 function Testimonials() {
   const { testimonials } = useSelector((state) => state.testimonials);
-  useEffect(() => {
-    console.log(testimonials);
-  }, [testimonials]);
+  useEffect(() => {}, [testimonials]);
 
   return (
     <section className='testimonial section'>
@@ -43,8 +41,8 @@ function Testimonials() {
           modules={[Pagination, Navigation]}
           className='mySwiper'
         >
-          {testimonials?.map((testimonial) => (
-            <SwiperSlide key={testimonial?.id}>
+          {testimonials?.map((testimonial, i) => (
+            <SwiperSlide key={i}>
               <div className='testimonial__card'>
                 <img
                   src={Testimonial1}
