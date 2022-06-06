1fe3045970f9c7bfda892246b897077acb1f8f29
Patricio Stickar <58763654+patostickar@users.noreply.github.com>
2022-06-06 20:11:08 -0300

    refactor: simplification of customer request handler

--- api/src/controllers/customers.js
+++ api/src/controllers/customers.js
@@ -1,154 +1,168 @@
-const { Customer } = require('../db');
+const { Customer, Review } = require('../db');
 
 const getCustomers = async (req, res, next) => {
   try {
-    const response = await Customer.findAll();
-    const data = response.map?.((res) => res.dataValues);
-    res.send(data);
+    const customers = await Customer.findAll();
+    if (!customers) res.send('No hay ningún cliente registrado');
+    res.send(customers);
   } catch (error) {
-    if (error.response) {
-      res.status(error.response.status).send({ msg: error.response.status });
-    } else if (error.request) {
-      next(error.request);
-    } else {
-      next(error);
-    }
-  }
-};
-const getCustomerByemail = async (req, res, next) => {
-  try {
-    const { email } = req.params;
-    if (email) {
-      const data = await Customer.findOne({where:{email}});
-      data
-        ? res.status(200).send(data)
-        : res.send({ msg: 'Customer not found' });
-    } else {
-      res.send({ msg: 'el email es necesario' });
-    }
-  } catch (error) {
-    if (error.response) {
-      res.status(error.response.status).send({ msg: error.response.status });
-    } else if (error.request) {
-      next(error.request);
-    } else {
-      next(error);
-    }
+    next(error);
   }
 };
 
+const getCustomerByemail = async (req, res, next) => {
+  const { email } = req.params;
+  if (!email) return res.status(400).send({ msg: 'el email es necesario' });
+  try {
+    const data = await Customer.findOne({ where: { email } });
+    data ? res.send(data) : res.status(400).send({ msg: 'Customer not found' });
+  } catch (error) {
+    next(error);
+  }
+};
 
 const postCustomer = async (req, res, next) => {
+  const {
+    email,
+    given_name: firstName,
+    family_name: lastName,
+    picture,
+  } = req.body;
   try {
-    const {
-      email,
-      given_name,
-      family_name,
-      picture,
-    } = req.body;
     const [customer, created] = await Customer.findOrCreate({
       where: {
         email,
-        img :picture,
-      },defaults:{
-        first_name : given_name,
-        last_name : family_name
-      }
+      },
+      defaults: {
+        firstName,
+        lastName,
+        img: picture,
+      },
     });
-    if (created) {
-      return res.status(201).send({ msg: 'Customer created' });
-    } else {
-      return res
-        .status(406)
-        .send({ msg: 'There is already a customer with this name', customer });
-    }
+
+    created
+      ? res.status(201).send({ msg: 'Cliente creado', customer })
+      : res.status(406).send('Ya existe un cliente con este correo');
   } catch (error) {
-    if (error.response) {
-      res.status(error.response.status).send({ msg: error.response.status });
-    } else if (error.request) {
-      next(error.request);
-    } else {
-      next(error);
-    }
+    next(error);
   }
 };
 
-const getCustomerById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    if (id) {
-      const data = await Customer.findByPk(id);
-      data
-        ? res.status(200).send(data)
-        : res.send({ msg: 'Customer not found by id' });
-    } else {
-      res.send({ msg: 'Customer not found' });
-    }
-  } catch (error) {
-    if (error.response) {
-      res.status(error.response.status).send({ msg: error.response.status });
-    } else if (error.request) {
-      next(error.request);
-    } else {
-      next(error);
-    }
-  }
-};
+// const getCustomerById = async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+//     if (id) {
+//       const data = await Customer.findByPk(id);
+//       data
+//         ? res.status(200).send(data)
+//         : res.send({ msg: 'Customer not found by id' });
+//     } else {
+//       res.send({ msg: 'Customer not found' });
+//     }
+//   } catch (error) {
+//     if (error.response) {
+//       res.status(error.response.status).send({ msg: error.response.status });
+//     } else if (error.request) {
+//       next(error.request);
+//     } else {
+//       next(error);
+//     }
+//   }
+// };
 
 const putCustomer = async (req, res, next) => {
+  const { id } = req.params;
+  if (!id) return res.status(400).send('Por favor enviar ID del cliente');
+  const {
+    email,
+    firstName,
+    lastName,
+    street,
+    city,
+    phone,
+    postalCode,
+    password,
+    isAdmin,
+    isPremium,
+    isBanned,
+  } = req.body;
+
   try {
-    const {
+    const customer = await Customer.findByPk(id);
+    if (!customer) return res.status(400).send('No hay un cliente con ese id');
+
+    await customer.update({
       email,
-      first_name,
-      last_name,
+      firstName,
+      lastName,
       street,
       city,
       phone,
-      postal_code,
+      postalCode,
       password,
       isAdmin,
       isPremium,
       isBanned,
-    } = req.body;
-    const { id } = req.params;
-    if (id) {
-      const data = await Customer.findOne({ where: { id } });
-      if (data) {
-        await data.update({
-          email,
-          first_name,
-          last_name,
-          street,
-          city,
-          phone,
-          postal_code,
-          password,
-          isAdmin,
-          isPremium,
-          isBanned,
-        });
-      }
-      data
-        ? res.status(200).send({ msg: 'Customer edited', data })
-        : res.send({ msg: 'Customer not found' });
-    } else {
-      res.send({ msg: 'Customer not found' });
-    }
+    });
+
+    res.status(200).send({ msg: 'Información editada', customer });
   } catch (error) {
-    if (error.response) {
-      res.status(error.response.status).send({ msg: error.response.status });
-    } else if (error.request) {
-      next(error.request);
-    } else {
-      next(error);
+    next(error);
+  }
+};
+
+const getReviews = async (req, res, next) => {
+  try {
+    // const {id1,id2,id3} =req.query
+    const testimonios = [];
+    const count = await Review.count();
+    const Getrandom = () => {
+      return Math.floor(Math.random() * (count - 1 + 1) + 1);
+    };
+    const id1 = Getrandom();
+    let id2 = Getrandom();
+    let id3 = Getrandom();
+    if (id2 === id1) {
+      id2 = Getrandom();
     }
+    if (id3 === id1 || id3 === id2) {
+      id3 = Getrandom();
+    }
+    testimonios.push(
+      await Review.findOne({ where: { id: id1 }, include: [Customer] })
+    );
+    testimonios.push(
+      await Review.findOne({ where: { id: id2 }, include: [Customer] })
+    );
+    testimonios.push(
+      await Review.findOne({ where: { id: id3 }, include: [Customer] })
+    );
+    res.send(testimonios);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const postReview = async (req, res, next) => {
+  const { id } = req.params;
+  const { reviews } = req.body;
+  try {
+    const review = await Review.create({ review: reviews });
+    const customer = await Customer.findOne({ where: { id } });
+    review.setCustomer(customer);
+    res.send('review Creada');
+  } catch (error) {
+    console.log(error);
+    next(error);
   }
 };
 
 module.exports = {
   getCustomers,
+  getCustomerByemail,
+  getReviews,
   postCustomer,
-  getCustomerById,
+  postReview,
   putCustomer,
-  getCustomerByemail
+  // getCustomerById,
 };
