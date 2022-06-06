abb57eaedb472b06ad04173c60808b810f0de1e3
Patricio Stickar <58763654+patostickar@users.noreply.github.com>
2022-06-06 20:09:52 -0300

    refactor: snake_case to camelCase

--- client/src/components/Account/PersonalInformation.jsx
+++ client/src/components/Account/PersonalInformation.jsx
@@ -10,8 +10,8 @@ import {
   Divider,
   Grid,
   TextField,
-   Typography,
-   Avatar
+  Typography,
+  Avatar,
 } from '@mui/material';
 
 // const states = [
@@ -41,192 +41,152 @@ import {
 export const PersonalInformation = () => {
   const { User } = useSelector((state) => state.user);
   const [values, setValues] = useState({
-    firstName: User.first_name,
-    lastName: User.last_name,
+    firstName: User.firstName,
+    lastName: User.lastName,
     email: User.email,
     phone: User.phone,
     city: User.city,
-    postal_code: User.postal_code,
-    avatar : User.img
+    postalCode: User.postalCode,
+    avatar: User.img,
   });
-
 
   const handleChange = (event) => {
     setValues({
       ...values,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   };
 
-  const handleSubmit = ()=>{
-    
-
-  };
+  const handleSubmit = () => {};
 
   return (
     <>
-   <Grid item xs={2}>
-
-   </Grid>
-    <Grid item xs={5} style={{ border:'solid 1px lightgrey', borderRadius:'8px'}}>
-     <Box style={{display:'flex', justifyContent:'center', padding:'10px'}} >
-        <Avatar
+      <Grid item xs={2}></Grid>
+      <Grid
+        item
+        xs={5}
+        style={{ border: 'solid 1px lightgrey', borderRadius: '8px' }}
+      >
+        <Box
+          style={{ display: 'flex', justifyContent: 'center', padding: '10px' }}
+        >
+          <Avatar
             src={values.avatar}
             sx={{
               height: 64,
               mb: 2,
               width: 64,
-               }}
+            }}
           />
-          </Box>
-    <form
-      autoComplete="off"
-      noValidate
-      onSubmit={handleSubmit}
-      
-    >
-      <Card>
-        <CardHeader
-          subheader="Actualiza tus datos"
-          title={
-            <Typography gutterBottom variant="h4">
-              Informacion Personal
-            </Typography>
-         }          
-        />
-       
-        <CardContent>
-          <Grid
-            container
-            spacing={3}
-          >
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                helperText="Please specify the first name"
-                label="First name"
-                name="firstName"
-                onChange={handleChange}
-                required
-                value={values.firstName}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                label="Last name"
-                name="lastName"
-                onChange={handleChange}
-                required
-                value={values.lastName}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                label="Email Address"
-                name="email"
-                onChange={handleChange}
-                required
-                value={values.email}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                label="Phone Number"
-                name="phone"
-                onChange={handleChange}
-                type="number"
-                value={values.phone}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                label="City"
-                name="city"
-                onChange={handleChange}
-                required
-                value={values.city}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                label="Street"
-                name="street"
-                onChange={handleChange}
-                required
-                value={values.street}
-                variant="outlined"
-              />
-            </Grid>
-            <Grid
-              item
-              md={6}
-              xs={12}
-            >
-              <TextField
-                fullWidth
-                label="Postal Code"
-                name="postalcode"
-                onChange={handleChange}
-                required
-                value={values.postal_code}
-                variant="outlined"
-              />
-            </Grid>
-          </Grid>
-        </CardContent>
-        
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            p: 2
-          }}
-        >
-          <Button
-            color="primary"
-            variant="contained"
-          >
-            Save details
-          </Button>
         </Box>
-      </Card>
-    </form>
-    </Grid>
+        <form autoComplete='off' noValidate onSubmit={handleSubmit}>
+          <Card>
+            <CardHeader
+              subheader='Actualiza tus datos'
+              title={
+                <Typography gutterBottom variant='h4'>
+                  Informacion Personal
+                </Typography>
+              }
+            />
+
+            <CardContent>
+              <Grid container spacing={3}>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    helperText='Please specify the first name'
+                    label='First name'
+                    name='firstName'
+                    onChange={handleChange}
+                    required
+                    value={values.firstName}
+                    variant='outlined'
+                  />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    label='Last name'
+                    name='lastName'
+                    onChange={handleChange}
+                    required
+                    value={values.lastName}
+                    variant='outlined'
+                  />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    label='Email Address'
+                    name='email'
+                    onChange={handleChange}
+                    required
+                    value={values.email}
+                    variant='outlined'
+                  />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    label='Phone Number'
+                    name='phone'
+                    onChange={handleChange}
+                    type='number'
+                    value={values.phone}
+                    variant='outlined'
+                  />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    label='City'
+                    name='city'
+                    onChange={handleChange}
+                    required
+                    value={values.city}
+                    variant='outlined'
+                  />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    label='Street'
+                    name='street'
+                    onChange={handleChange}
+                    required
+                    value={values.street}
+                    variant='outlined'
+                  />
+                </Grid>
+                <Grid item md={6} xs={12}>
+                  <TextField
+                    fullWidth
+                    label='Postal Code'
+                    name='postalcode'
+                    onChange={handleChange}
+                    required
+                    value={values.postal_code}
+                    variant='outlined'
+                  />
+                </Grid>
+              </Grid>
+            </CardContent>
+
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                p: 2,
+              }}
+            >
+              <Button color='primary' variant='contained'>
+                Save details
+              </Button>
+            </Box>
+          </Card>
+        </form>
+      </Grid>
     </>
   );
 };
-
