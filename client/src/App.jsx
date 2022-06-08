f84ade15fc68e42551b3825bbbb88a6b33674def
Patricio Stickar <58763654+patostickar@users.noreply.github.com>
2022-06-08 19:00:20 -0300

    admin protected routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import { fetchTestimonials } from './redux/testimonials';
 import { ClearUser, fetchUser } from './redux/user.js';
 import { Routes, Route } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
-import { ProtectedRoute } from './components/protected-route';
+import { ProtectedRoute } from './protected-route';
 import axios from 'axios';
 import logError from './components/GeneralFuntions/logError';
 import About from './pages/About';
@@ -18,6 +18,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import LocationCreate from './components/AdminPanel/CreateForms/LocationCreate';
 import Navbar from './components/Navbar';
+import NotAllowed from './pages/NotAllowed';
 import NotFound from './pages/NotFound';
 import Reservation from './pages/Reservation';
 import Response from './pages/MPrespose';
@@ -62,18 +63,19 @@ function App() {
         {/* componenete de adminPanel */}
         <Route
           path='/carcreate'
-          element={<ProtectedRoute component={CarCreate} />}
+          element={<ProtectedRoute component={CarCreate} role='admin' />}
         />
         {/* componenete de adminPanel */}
         <Route
           path='/cartypecreate'
-          element={<ProtectedRoute component={CarTypeCreate} />}
+          element={<ProtectedRoute component={CarTypeCreate} role='admin' />}
         />
         {/* componenete de adminPanel */}
         <Route
           path='/locationcreate'
-          element={<ProtectedRoute component={LocationCreate} />}
+          element={<ProtectedRoute component={LocationCreate} role='admin' />}
         />
+        <Route path='/notAllowed' element={<NotAllowed />} />
         <Route
           path='/profile'
           element={<ProtectedRoute component={Account} />}
--- client/src/protected-route.jsx
+++ client/src/protected-route.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { withAuthenticationRequired } from '@auth0/auth0-react';
-import { Loader } from './Loader';
+import { Loader } from './components/Loader';
 
 export const ProtectedRoute = ({ component, role }) => {
   const Component = withAuthenticationRequired(component, {
@@ -15,7 +15,7 @@ export const ProtectedRoute = ({ component, role }) => {
   useEffect(() => {
     if (user) {
       if (role === 'admin' && !user.isAdmin) {
-        navigate('/');
+        navigate('/notAllowed');
       }
     }
   });
