076f6bdfd5f455166df8d5992dd604faf0a1f65b
franco garcia <96747065+Dakkai@users.noreply.github.com>
2022-06-09 16:28:11 -0300

    Update mailer.js

--- api/src/services/mailer.js
+++ api/src/services/mailer.js
@@ -6,17 +6,17 @@ const {MAIL_APP_PASS } = process.env;
  const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
-    secure: true, // true for 465, false for other ports
+    secure: true, 
     auth: {
-      user: "carrycars.info@gmail.com", // generated ethereal user
-      pass: MAIL_APP_PASS, // generated ethereal password
+      user: "carrycars.info@gmail.com", 
+      pass: MAIL_APP_PASS, 
     },
   });
 
  const sendMAil = async (email) => { await transporter.sendMail({
-    from: 'CarryCars <carrycars.info@gmail.com>', // sender address
-    to: `${email}`, // list of receivers
-    subject: "Gracias Por confiar en Carry", // Subject line
+    from: 'CarryCars <carrycars.info@gmail.com>', 
+    to: `${email}`, 
+    subject: "Gracias Por confiar en Carry", 
     //  text: "Hello world?", // plain text body
     html: "<h1> gracias por su compra </h1>", // html body
   })}
