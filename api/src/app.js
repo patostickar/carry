be74aad4d87fe9ea65efa9eaad1f95adca79605b
Patricio Stickar <58763654+patostickar@users.noreply.github.com>
2022-06-10 20:31:44 -0300

    fix: dotenv was not loading properly

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
-const dotenv = require('dotenv');
 const server = express();
 
-dotenv.config();
 require('./db.js');
 
 server.name = 'API';
