ffc60f3fb2dbf5f5af3d906dfdcab1c87684064b
Patricio Stickar <58763654+patostickar@users.noreply.github.com>
2022-06-12 20:00:18 -0300

    fix: se ven los datos luego de actualizar

--- client/src/redux/user.js
+++ client/src/redux/user.js
@@ -3,15 +3,16 @@ import axios from 'axios';
 import Swal from 'sweetalert2';
 
 const initialState = {
-  token: null,
-  email: null,
-  firstName: null,
-  lastName: null,
-  street: null,
-  city: null,
-  postalCode: null,
-  phone: null,
-  img: null,
+  id: '',
+  token: '',
+  email: '',
+  firstName: '',
+  lastName: '',
+  street: '',
+  city: '',
+  postalCode: '',
+  phone: '',
+  img: '',
 };
 
 export const user = createSlice({
@@ -30,11 +31,11 @@ export const user = createSlice({
 export const putUser = (id, data) => async (dispatch) => {
   try {
     await axios.put(`/customers/${id}`, data).then((res) => {
-      dispatch(setUser(res.data));
-      Swal.fire('La informacion se ha actualizado correctamente!!');
+      dispatch(setUser(res.data.customer));
+      Swal.fire(res.data.msg);
     });
   } catch (error) {
-    Swal.fire('Error: ', error.message);
+    Swal.fire('Error: ', error.response.data);
     console.log(error);
   }
 };
