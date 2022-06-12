d321a590a64ef94d5c041f5dacd34063cec21d70
ByronCorrea <byron10-@hotmail.com>
2022-06-12 14:55:45 -0300

    Update button styles

--- client/src/components/MercadoPago/Checkout.jsx
+++ client/src/components/MercadoPago/Checkout.jsx
@@ -1,21 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect } from "react";
+import styles from "./styles/Checkout.module.css";
 
-const Checkout = ({ data ,id }) => {
+const Checkout = ({ data, id }) => {
   useEffect(() => {
-    const script = document.createElement('script');
-    const attrDataPreference = document.createAttribute('data-preference-id');
+    const script = document.createElement("script");
+    const attrDataPreference = document.createAttribute("data-preference-id");
     attrDataPreference.value = data.id;
 
     script.src =
-      'https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js';
+      "https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js";
     script.setAttributeNode(attrDataPreference);
 
     document.getElementById(id).appendChild(script);
   }, [data]);
 
-  return <form id={id}></form>;
-
- 
+  return <form className={styles.form} id={id}></form>;
 };
 
 export default Checkout;
