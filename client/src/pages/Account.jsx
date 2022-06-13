7da8dd30503cd12fbf56a3e81b2fa8d5828e2df8
ByronCorrea <byron10-@hotmail.com>
2022-06-13 12:10:25 -0300

    Update TabTitle

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,42 +1,42 @@
+import React, { useState } from "react";
+import { Grid } from "@mui/material";
+import { Sidebar } from "../components/Account/Sidebar";
+import { PersonalInformation } from "../components/Account/PersonalInformation";
+import Review from "../components/Account/Review";
+import { Booking } from "../components/Account/Booking";
+import { UserCard } from "../components/Account/UserCard";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import TabTitle from "../components/GeneralFuntions/TabTitle";
 
-import React, { useState } from 'react';
-import { Grid } from '@mui/material';
-import { Sidebar } from '../components/Account/Sidebar';
-import { PersonalInformation } from '../components/Account/PersonalInformation';
-import Review from '../components/Account/Review';
-import { Booking } from '../components/Account/Booking';
-import { UserCard } from '../components/Account/UserCard';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
 export default function Account() {
+  TabTitle("Perfil");
+
   const [renderControl, setRenderControl] = useState({
     personalInfo: false,
     review: false,
     booking: false,
-    userCard:true,
-    
+    userCard: true,
   });
   return (
     <>
-    <Navbar />
+      <Navbar />
       <Grid container my={4}>
         <Sidebar
           setRenderControl={setRenderControl}
           renderControl={renderControl}
         />
         {renderControl.userCard && (
-          <UserCard 
-          setRenderControl={setRenderControl}
-          renderControl={renderControl}/>
-          
+          <UserCard
+            setRenderControl={setRenderControl}
+            renderControl={renderControl}
+          />
         )}
-        { renderControl.personalInfo && <PersonalInformation />
-        }
+        {renderControl.personalInfo && <PersonalInformation />}
         {renderControl.booking && <Booking />}
         {renderControl.review && <Review />}
-        
       </Grid>
-      <Footer/>
+      <Footer />
     </>
   );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,14 +1,14 @@
-import SearchBarJumbo from '../components/Home/SearchBarJumbo';
-import PopularLocations from '../components/Home/PopularLocations';
-import Testimonials from '../components/Home/Testimonials';
-import FAQ from '../components/Home/FAQ';
-import TabTitle from '../components/GeneralFuntions/TabTitle';
-import styles from './styles/Home.module.css';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import SearchBarJumbo from "../components/Home/SearchBarJumbo";
+import PopularLocations from "../components/Home/PopularLocations";
+import Testimonials from "../components/Home/Testimonials";
+import FAQ from "../components/Home/FAQ";
+import TabTitle from "../components/GeneralFuntions/TabTitle";
+import styles from "./styles/Home.module.css";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Home = () => {
-  TabTitle('Inicio - Carry');
+  TabTitle("Inicio");
   return (
     <>
       <Navbar />
