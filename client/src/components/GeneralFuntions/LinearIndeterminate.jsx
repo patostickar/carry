3060843dad8d8a84669a2661d54754a2d77be8be
franco garcia <96747065+Dakkai@users.noreply.github.com>
2022-06-13 11:26:21 -0300

    mensaje que indica que no hay autos disponibles, faltan estilos

--- client/src/components/GeneralFuntions/LinearIndeterminate.jsx
+++ client/src/components/GeneralFuntions/LinearIndeterminate.jsx
@@ -1,11 +1,15 @@
 import Box from "@mui/material/Box";
 import LinearProgress from "@mui/material/LinearProgress";
 
-export default function LinearIndeterminate() {
+export default function LinearIndeterminate({cartypes}) {
   return (
     <Box sx={{ width: "805px" }}>
+      {cartypes !== 0 ? (<>
       <LinearProgress />
-      <h1>Buscando los mejores autos para vos... 🚘</h1>
+      <h1>Buscando los mejores autos para vos... 🚘</h1></>):
+      <div>no tenemos autos disponibles en esta hubicacion</div>}
+     
     </Box>
+   
   );
 }
