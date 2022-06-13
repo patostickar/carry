8071e163c759117664e1496d121f42e151ac2935
ByronCorrea <byron10-@hotmail.com>
2022-06-13 12:08:35 -0300

    Update route booking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,27 @@
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchAllLocations } from './redux/searchBar';
-import { fetchTestimonials } from './redux/testimonials';
-import { clearUser, setUser } from './redux/user.js';
-import { Routes, Route } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
-import { ProtectedRoute } from './protected-route';
-import axios from 'axios';
-import About from './pages/About';
-import Account from './pages/Account';
-import AdminCard from './components/AdminPanel/AdminPanelCard';
-import CarCreate from './components/AdminPanel/CreateForms/CarCreate';
-import CarTypeCreate from './components/AdminPanel/CreateForms/CarTypeCreate';
-import Home from './pages/Home';
-import LocationCreate from './components/AdminPanel/CreateForms/LocationCreate';
-import NotAllowed from './pages/NotAllowed';
-import NotFound from './pages/NotFound';
-import Reservation from './pages/Reservation';
-import Response from './pages/MPrespose';
-import SearchResults from './pages/SearchResults';
-import TermsAndConditions from './pages/TermsAndConditions/';
-import User from './components/AdminPanel/AdminPages/AdminComponents/sections/User';
-import BookingsAdmin from './components/AdminPanel/AdminPages/AdminComponents/sections/layouts/BookingsAdmin';
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchAllLocations } from "./redux/searchBar";
+import { fetchTestimonials } from "./redux/testimonials";
+import { clearUser, setUser } from "./redux/user.js";
+import { Routes, Route } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { ProtectedRoute } from "./protected-route";
+import axios from "axios";
+import About from "./pages/About";
+import Account from "./pages/Account";
+import AdminCard from "./components/AdminPanel/AdminPanelCard";
+import CarCreate from "./components/AdminPanel/CreateForms/CarCreate";
+import CarTypeCreate from "./components/AdminPanel/CreateForms/CarTypeCreate";
+import Home from "./pages/Home";
+import LocationCreate from "./components/AdminPanel/CreateForms/LocationCreate";
+import NotAllowed from "./pages/NotAllowed";
+import NotFound from "./pages/NotFound";
+import Booking from "./pages/Booking";
+import Response from "./pages/MPrespose";
+import SearchResults from "./pages/SearchResults";
+import TermsAndConditions from "./pages/TermsAndConditions/";
+import User from "./components/AdminPanel/AdminPages/AdminComponents/sections/User";
+import BookingsAdmin from "./components/AdminPanel/AdminPages/AdminComponents/sections/layouts/BookingsAdmin";
 
 function App() {
   const dispatch = useDispatch();
@@ -33,8 +33,8 @@ function App() {
     } else {
       async function setCustomer() {
         try {
-          const res = await axios.post('/customers', user);
-          localStorage.setItem('token', `Bearer ${res.headers.authorization}`);
+          const res = await axios.post("/customers", user);
+          localStorage.setItem("token", `Bearer ${res.headers.authorization}`);
           dispatch(setUser(res.data.customerDetails));
         } catch (error) {
           console.log(error);
@@ -52,39 +52,39 @@ function App() {
   return (
     <Routes>
       <Route
-        path='/adminPanel'
-        element={<ProtectedRoute component={AdminCard} role='admin' />}
+        path="/adminPanel"
+        element={<ProtectedRoute component={AdminCard} role="admin" />}
       />
       <Route
-        path='/cartypecreate'
-        element={<ProtectedRoute component={CarTypeCreate} role='admin' />}
+        path="/cartypecreate"
+        element={<ProtectedRoute component={CarTypeCreate} role="admin" />}
       />
       <Route
-        path='/locationcreate'
-        element={<ProtectedRoute component={LocationCreate} role='admin' />}
+        path="/locationcreate"
+        element={<ProtectedRoute component={LocationCreate} role="admin" />}
       />
       <Route
-        path='/user'
-        element={<ProtectedRoute component={User} role='admin' />}
+        path="/user"
+        element={<ProtectedRoute component={User} role="admin" />}
       />
-      <Route path='/bookingsadmin' element={<BookingsAdmin />} />
+      <Route path="/bookingsadmin" element={<BookingsAdmin />} />
 
       <Route
-        path='/carcreate'
-        element={<ProtectedRoute component={CarCreate} role='admin' />}
+        path="/carcreate"
+        element={<ProtectedRoute component={CarCreate} role="admin" />}
       />
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/notAllowed' element={<NotAllowed />} />
-      <Route path='/profile' element={<ProtectedRoute component={Account} />} />
-      <Route path='/reservation' element={<Reservation />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/notAllowed" element={<NotAllowed />} />
+      <Route path="/profile" element={<ProtectedRoute component={Account} />} />
+      <Route path="/booking" element={<Booking />} />
       <Route
-        path='/Response'
+        path="/Response"
         element={<ProtectedRoute component={Response} />}
       />
-      <Route path='/searchResult' element={<SearchResults />} />
-      <Route path='/terminos-condiciones' element={<TermsAndConditions />} />
-      <Route path='*' element={<NotFound />} />
+      <Route path="/searchResult" element={<SearchResults />} />
+      <Route path="/terminos-condiciones" element={<TermsAndConditions />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
