29264cb7ec233c9d8151c08f9567ffabb0f52d72
ByronCorrea <byron10-@hotmail.com>
2022-06-14 15:39:50 -0300

    Responsive

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,13 @@
-import { useNavigate } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchCarTypes } from '../../redux/carsResults';
-import { DAY_MILISECONDS } from '../GeneralFuntions/constants';
-import Location from './locationInput';
-import Calendar from './calendar';
-import styles from './styles/SearchBar.module.css';
-import 'sweetalert2/dist/sweetalert2.css';
-import Alerts from '../GeneralFuntions/Alerts';
-import { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCarTypes } from "../../redux/carsResults";
+import { DAY_MILISECONDS } from "../GeneralFuntions/constants";
+import Location from "./locationInput";
+import Calendar from "./calendar";
+import styles from "./styles/SearchBar.module.css";
+import "sweetalert2/dist/sweetalert2.css";
+import Alerts from "../GeneralFuntions/Alerts";
+// import { useState } from "react";
 
 function SearchBar() {
   const { pickupDate, dropoffDate, location, popLocation } = useSelector(
@@ -17,32 +17,34 @@ function SearchBar() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-
-
   const handleSearch = () => {
- 
     if (!location) {
-      return Alerts('warning', 'Por favor elija una ubicación');
+      return Alerts("warning", "Por favor elija una ubicación");
     }
-  
-   if(new Date(pickupDate).getUTCDate() === new Date().getUTCDate()){
-     return Alerts('info', 'El alquiler mínimo es de 24 hs')}
+
+    if (new Date(pickupDate).getUTCDate() === new Date().getUTCDate()) {
+      return Alerts("info", "El alquiler mínimo es de 24 hs");
+    }
     if (dropoffDate - pickupDate < DAY_MILISECONDS) {
-      return Alerts('info', 'El alquiler mínimo es de 24 hs');
+      return Alerts("info", "El alquiler mínimo es de 24 hs");
     }
     dispatch(fetchCarTypes(location.id, pickupDate, dropoffDate));
-    navigate('/searchResult',{state:location});
-    console.log(pickupDate)
-    console.log(dropoffDate)
+    navigate("/searchResult", { state: location });
+    console.log(pickupDate);
+    console.log(dropoffDate);
   };
 
   return (
     <>
       <div className={styles.headerSearch}>
         <div className={styles.headerSearchItem}>
-          <Location type='Ubicación' popLocation={popLocation} />
+          <Location type="Ubicación" popLocation={popLocation} />
         </div>
-        <Calendar />
+        <div className={styles.calendar}>
+          {" "}
+          <Calendar />
+        </div>
+
         <div className={styles.headerSearchItem}>
           <button className={styles.siCheckButton} onClick={handleSearch}>
             Buscar
