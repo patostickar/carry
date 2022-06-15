f88c702fc64a04ff0155724389dd3a4f8e391279
harshtiger <ttiger6666@gmail.com>
2022-06-15 18:14:01 -0300

    Fixes finales

--- client/src/components/AdminPanel/AdminPages/AdminComponents/sections/User.jsx
+++ client/src/components/AdminPanel/AdminPages/AdminComponents/sections/User.jsx
@@ -97,13 +97,13 @@ function applySortFilter(array, comparator, query) {
     if (order !== 0) return order;
     return a[1] - b[1];
   });
-  if (query) {
+  if ( query ) {
     // if(query > 0){
     
     return filter(
       array,
       
-      (_user) => _user.firstName.toLowerCase().indexOf(query.toLowerCase()) !== -1
+      (_user) => _user.firstName?.toLowerCase().indexOf(query?.toLowerCase()) !== -1
       
     ); 
   // }
@@ -337,7 +337,7 @@ export default function User() {
                                 isAdmin,
                               } = row;
                               const isItemSelected =
-                                selected.indexOf(firstName) !== -1;
+                                selected.indexOf(id) !== -1;
 
                               return (
                                 <TableRow
@@ -384,7 +384,7 @@ export default function User() {
                                   </TableCell>
 
                                   <TableCell align='right'>
-                                    <UserMoreMenu />
+                                    
                                   </TableCell>
                                 </TableRow>
                               );
